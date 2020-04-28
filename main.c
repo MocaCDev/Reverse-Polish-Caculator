5eec8f5d3870650f7bf9b0b4c933c99d364df8be
Scott Little <k1206150@students.katyisd.org>
2020-04-28 14:22:07 +0000

    Made pop() better and change .replit to use GCC for GDB

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+//#include <readline.h>
+//TODO: Link against libreadline for input.
 
 /* 
 	@CodeLongAndPros:
@@ -6,27 +8,43 @@
 		Sorry if it is a bit messy..also I left a few comments about some things we should add, and I changed the assignment of ret in pop.
 
 		You can delete all the comments if you want.
+		Also...I created a GitHub repo:
+			- https://github.com/ARACADERISE/Reverse-Polish-Caculator
+
+  @CTALENT:
+
 */
 
 int stack[100];
-int p; /*
-	Possible TO-DO: Make this static?
-	Possible TO-DO: Assign it defaulty to 0 instead of assigning it in main?
+int p =0; /*
+	Possible TO-DO: Make this static? This is the stack pointer, and needs to be global.
 */
 
 void push(int val) {
 	// So we are only pushing values less than 100?
+  // We can only have 100 vals in the stack. 
   if (p < 100) {
     stack[p] = val;
     p++; /* 
 			So, if p=2 and val=100, then stack[2]=100, then p will be 3 afterwards?
 			My understanding: Since we are pushing, we are going to be increasing the index each time we push..right?
+      Yes
 		*/
   }
 }
-
+void dump() {
+  printf("%s\n", "START");
+  for( int i = 0; i < p; ++i) {
+    printf("%d\t%d\n", stack[i], i);
+  }
+  printf("%s\n", "END");
+}
+void clear() {
+  stack[p] = 0;
+}
 int pop() {
-  int ret = stack[p-2]; /* 
+  clear();
+  return stack[(--p)]; /* 
 		Are we wanting ret to be zero?
 		We have to remember, we are increasing it in push, and each value of the array stack is
 		defaulty zero..so we assigned index of p to value of val, then we increased p to the next index..being
@@ -41,15 +59,16 @@ int pop() {
 
 	// Since we are removing the last index, we should re-assign it to its default value..zero
 	// Possible TO-DO: stack[p-1]=0;?
-  p--;
-  return ret;
 }
 
 int main(void) {
-  p=0; // We can just assign it defaulty when creating the global variable...
   push(1);
   push(3);
 	// See, if we push(4), then stack[p-2] would then be 3
 	push(4);
+  dump();
   printf("%d\n", pop());
+  printf("%d\n", pop());
+  printf("%d\n", pop());
+  
 }
